cbfd6c3c63aec35ac11094491a08467369cd4d1f
Demir Antay <demir99antay@gmail.com>
2022-06-22 11:22:15 +0300

    srated working on the data nodes

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Node: Image Tiles
+
+// Node: Info Sheet
+var info_sheet = document.getElementById("info_sheet");
+var info_sheet_header = document.getElementById("info_sheet_header");
+var info_sheet_text = document.getElementById("info_sheet_text");
+
+// Node: Start
+
+// Node: Randomize
+
+// Node: Reset
+
+// Node: Speed
+
+// Node: Tile Division
+
+// Node: Selected Algorithm
+
+
+// ------------------
+
+
+// Algo: Image Tiles
+
+// Algo: Info Sheet
+
+// Algo: Start
+
+// Algo: Randomize
+
+// Algo: Reset
+
+// Algo: Speed
+
+// Algo: Tile Division
+
+// Algo: Selected Algorithm
