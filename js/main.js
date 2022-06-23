6a8a2664e1b602c5bb958e949b1da693c170fe02
Demir Antay <demir99antay@gmail.com>
2022-06-23 11:30:26 +0300

    started adding data structures with javascript

--- js/main.js
+++ js/main.js
@@ -6,19 +6,43 @@ var info_sheet_header = document.getElementById("info_sheet_header");
 var info_sheet_text = document.getElementById("info_sheet_text");
 
 // Node: Start
+var start_button = document.getElementById("start-btn");
 
 // Node: Randomize
+var random_button = document.getElementById("random-btn");
 
 // Node: Reset
+var reset_button = document.getElementById("reset-btn");
 
 // Node: Speed
+var speed_1x = document.getElementById("speed_1x");
+var speed_2x = document.getElementById("speed_2x");
+var speed_4x = document.getElementById("speed_4x");
+var speed_8x = document.getElementById("speed_8x");
 
 // Node: Tile Division
 
 // Node: Selected Algorithm
+var bubble_sort = document.getElementById("bubble_sort");
+var selection_sort = document.getElementById("selection_sort");
+var insertion_sort = document.getElementById("insertion_sort");
+var heap_sort = document.getElementById("heap_sort");
+var merge_sort = document.getElementById("merge_sort");
+var quick_sort = document.getElementById("quick_sort");
+var shell_sort = document.getElementById("shell_sort");
+var counting_sort = document.getElementById("counting_sort");
+var radix_sort = document.getElementById("radix_sort");
+
+// Node: Selected Image and Text
+
+// Node: Info Sheet
+var painting_info = document.getElementById("painting_info");
+var algorithm_info = document.getElementById("algorithm_info");
+var division_info = document.getElementById("division_info");
+var speed_info = document.getElementById("speed_info");
 
 
-// ------------------
+// --------------------------------------------------------------------
 
 
 // Algo: Image Tiles
@@ -26,12 +50,43 @@ var info_sheet_text = document.getElementById("info_sheet_text");
 // Algo: Info Sheet
 
 // Algo: Start
+start_button.onclick = function() {
+  alert("start");
+}
 
 // Algo: Randomize
+random_button.onclick = function() {
+  alert("random");
+}
 
 // Algo: Reset
+reset_button.onclick = function() {
+  alert("reset");
+}
+
+// Algo Info Sheet
 
 // Algo: Speed
+speed_1x.onclick = function(event) {
+  event.preventDefault();
+  alert("speed 1x");
+}
+
+speed_2x.onclick = function(event) {
+  event.preventDefault();
+  alert("speed 2x");
+}
+
+speed_4x.onclick = function(event) {
+  event.preventDefault();
+  alert("speed 4x");
+}
+
+speed_8x.onclick = function(event) {
+  event.preventDefault();
+  alert("speed 8x");
+}
+
 
 // Algo: Tile Division
 
